58c54ebdd923cce9b8dc578428ef40f708f5d21a
Chen Di <sundychenya1018@yahoo.com>
2017-10-01 04:32:28 -0400

    Integrate webcam into canvas

--- libs/core/sims.d.ts
+++ libs/core/sims.d.ts
@@ -1,23 +1,13 @@
 // Auto-generated from simulator. Do not edit.
-declare namespace turtle {
+declare namespace faceAR {
     /**
      * Moves the sprite forward
      * @param steps number of steps to move, eg: 1
      */
     //% weight=90
     //% block
-    //% shim=turtle::forwardAsync promise
-    function forward(steps: number): void;
-
-    /**
-     * Moves the sprite forward
-     * @param direction the direction to turn, eg: Direction.Left
-     * @param angle degrees to turn, eg:90
-     */
-    //% weight=85
-    //% blockId=sampleTurn block="turn %direction|by %angle degrees"
-    //% shim=turtle::turnAsync promise
-    function turn(direction: Direction, angle: number): void;
+    //% shim=faceAR::drawLoopAsync promise
+    function drawLoop(): void;
 
 }
 declare namespace loops {
@@ -47,42 +37,6 @@ declare namespace console {
     //%
     //% shim=console::log
     function log(msg: string): void;
-
-}
-    /**
-     * A ghost on the screen.
-     */
-    //%
-    declare class Sprite {
-        /**
-         * The X-coordiante
-         */
-        //%
-        //% shim=.x
-        public x: number;
-
-        /**
-         * The Y-coordiante
-         */
-        //%
-        //% shim=.y
-        public y: number;
-
-        /**
-         * Move the thing forward
-         */
-        //%
-        //% shim=.forwardAsync promise
-        public forward(steps: number): void;
-
-    }
-declare namespace sprites {
-    /**
-     * Creates a new sprite
-     */
-    //% block
-    //% shim=sprites::createSprite
-    function createSprite(): Sprite;
 
 }
 
--- sim/simulator.ts
+++ sim/simulator.ts
@@ -1,47 +1,99 @@
 /// <reference path="../node_modules/pxt-core/typings/globals/bluebird/index.d.ts"/>
 /// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
 
+interface Navigator {
+    getUserMedia(
+        options: { video?: boolean; audio?: boolean; },
+        success: (stream: any) => void,
+        error?: (error: string) => void
+    ) : void;
+}
+
 namespace pxsim {
     /**
      * This function gets called each time the program restarts
      */
     initCurrentRuntime = () => {
-        runtime.board = new Board();
+        runtime.board = new FaceDetector();
     };
 
     /**
      * Gets the current 'board', eg. program state.
      */
-    export function board() : Board {
-        return runtime.board as Board;
+    export function faceDetector() : FaceDetector {
+        return runtime.board as FaceDetector;
     }
 
     /**
      * Represents the entire state of the executing program.
      * Do not store state anywhere else!
      */
-    export class Board extends pxsim.BaseBoard {
-        public element : SVGSVGElement;
-        public spriteElement: SVGCircleElement;
-        public sprite : Sprite;
-        
+    export class FaceDetector extends pxsim.BaseBoard{
+        public vid: any;
+        public vid_width: number;
+        public vid_height: number;
+        public overlay: any;
+        public overlayCC: any;
+
+        public clm: any;
+        public clmtrackr: any;
+        public facePositionArray : Array<Array<number>>;
+
         constructor() {
             super();
-            this.element = <SVGSVGElement><any>document.getElementById('svgcanvas');
-            this.spriteElement = <SVGCircleElement>this.element.getElementById('svgsprite');
-            this.sprite = new Sprite()
+            this.vid = <any>document.getElementById('videoel');
+            this.vid_width = this.vid.width;
+            this.vid_height = this.vid.height;
+            this.overlay = <any>document.getElementById('overlay');
+            this.overlayCC = <any>this.overlay.getContext('2d');
+            //this.clm = <any>clm;
         }
-        
-        initAsync(msg: pxsim.SimulatorRunMessage): Promise<void> {
-            document.body.innerHTML = ''; // clear children
-            document.body.appendChild(this.element);
 
+        //drawLoop() {
+        //    requestAnimationFrame(this.drawLoop);
+        //    this.overlayCC.clearRect(0, 0, this.vid_width, this.vid_height);
+        //    if (this.clmtrackr.getCurrentPosition()) {
+        //        this.clmtrackr.draw(this.overlay);
+        //    }
+        //    return Promise.delay(400);
+        //}
+
+        drawLoop() {
+            // do nothing
+            return Promise.delay(400);
+        }
+
+        //gumSuccess(stream: any) {
+        //    // add camera stream if getUserMedia succeeded
+        //    this.vid.src = window.URL.createObjectURL(stream);
+        //    this.vid.play();
+        //}
+        //
+        //gumFail() {
+        //    alert("There was some problem trying to fetch video from your webcam");
+        //}
+
+        initAsync(msg: pxsim.SimulatorRunMessage): Promise<void> {
+
+            // window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
+            // Get video
+            navigator.getUserMedia({video : true}, function(stream: any) {
+                this.vid.src = window.URL.createObjectURL(stream);
+                this.vid.play();
+            }.bind(this), function() {
+                //alert("There was some problem trying to fetch video from your webcam");
+            });
+            /*
+            this.clmtrackr =  this.clm.tracker();
+            this.clmtrackr.init();
+            this.clmtrackr.start(this.vid);
+
+            this.drawLoop();
+            */
             return Promise.resolve();
-        }       
-        
+        }
+
         updateView() {
-            this.spriteElement.cx.baseVal.value = this.sprite.x;
-            this.spriteElement.cy.baseVal.value = this.sprite.y;
         }
     }
 }
