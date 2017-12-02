d229fd95151931997060644b08e489ed8134d5ae
JCSPEC <JC@ChundeMacBook-Pro.local>
2017-12-01 20:37:02 -0500

    logo update

--- libs/core/built/debug/binary.js
+++ libs/core/built/debug/binary.js
@@ -1,6 +1,5 @@
 pxsim.noRefCounting();
 pxsim.enableFloatingPoint();
-pxsim.setConfigData({}, {});
 
 
 var _main___P1 = entryPoint = function (s) {
@@ -11,7 +10,12 @@ if (yieldSteps-- < 0 && maybeYield(s, step, r0)) return null;
 switch (step) {
   case 0:
 
-    r0 = pxsim.loops.forever(inline__P62);
+    r0 = inline__P20;
+    s.tmp_0 = r0;
+    r0 = pxsim.loops.forever(s.tmp_0);
+    s.tmp_1 = r0;
+    r0 = s.tmp_0;
+    r0 = s.tmp_1;
   case 1:
     return leave(s, r0)
   default: oops()
@@ -21,7 +25,7 @@ _main___P1.continuations = [  ]
 
 
 
-var inline__P62  = function (s) {
+var inline__P20  = function (s) {
 var r0 = s.r0, step = s.pc;
 s.pc = -1;
 while (true) {
@@ -47,7 +51,7 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-inline__P62.info = {"start":477,"length":200,"line":33,"column":14,"endLine":44,"endColumn":1,"fileName":"ns.ts","functionName":"inline"}
+inline__P20.info = {"start":444,"length":189,"line":33,"column":14,"endLine":44,"endColumn":1,"fileName":"ns.ts","functionName":"inline"}
 
 
 setupDebugger(1)
