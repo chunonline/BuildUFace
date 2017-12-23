2d9206586ab76c38ee003bfb3b309d8013970a32
Chen Di <sundychenya1018@yahoo.com>
2017-12-22 23:33:00 -0500

    Auto-push

---
+++ monacoworker.js
@@ -0,0 +1,5 @@
+// see https://github.com/Microsoft/monaco-editor
+self.MonacoEnvironment = { 
+    baseUrl: "/Microsoft_MakeCode/" 
+};
+importScripts("/Microsoft_MakeCode/vs/base/worker/workerMain.js");
