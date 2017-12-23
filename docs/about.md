9f5ea28ba9efaa89495ced9ae94f954297e5f916
Chen Di <sundychenya1018@yahoo.com>
2017-12-22 23:32:56 -0500

    Initial.

--- docs/about.md
+++
@@ -1,5 +0,0 @@
-# About Sample Target
-
-Example of editor target for [Microsoft MakeCode](https://makecode.com/).
-
-See [GitHub repo](https://github.com/Microsoft/pxt-sample) for details.
--- monacoworker.js
+++
@@ -1,5 +0,0 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/Microsoft_MakeCode/" 
-};
-importScripts("/Microsoft_MakeCode/vs/base/worker/workerMain.js");
